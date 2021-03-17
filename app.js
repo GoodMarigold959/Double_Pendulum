b2ee1f751cacdb055d9a32d5058f9063e1d7a615
Oboe Reed <31904648+GoodMarigold959@users.noreply.github.com>
2021-03-17 19:11:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 let background;
 let context;
+let startbutton;
+let drawbutton;
+
+let yp1;
+let yp2;
+let rp1;
+let rp2;
+let bp1;
+let bp2;
 
 let foreground;
 let ball0;
@@ -16,7 +25,7 @@ let time = 0.05;
 let d2Theta1 = 0 * (Math.PI) / 180;
 let d2Theta2 = 0 * (Math.PI) / 180;
 let dTheta1 = 0 * (Math.PI) / 180;
-let dTheta2 = 1.02 * (Math.PI) / 180;
+let dTheta2 = 1.000002 * (Math.PI) / 180;
 let Theta1 = 90 * (Math.PI) / 180;
 let Theta2 = 270 * (Math.PI) / 180;
 let m1 = 10;
@@ -38,7 +47,7 @@ let arm2_2;
 let d2Theta1_2 = 0 * (Math.PI) / 180;
 let d2Theta2_2 = 0 * (Math.PI) / 180;
 let dTheta1_2 = 0 * (Math.PI) / 180;
-let dTheta2_2 = 1.01 * (Math.PI) / 180;
+let dTheta2_2 = 1.000001 * (Math.PI) / 180;
 let Theta1_2 = 90 * (Math.PI) / 180;
 let Theta2_2 = 270 * (Math.PI) / 180;
 let m1_2 = 10;
@@ -118,22 +127,59 @@ function start() {
     arm1_3.setAttribute("x1", X0);
     arm1_3.setAttribute("y1", Y0);
 
-    animate();
+    draw();
 
     //context = background.getContext("2d");
     //context.beginPath();
     //context.strokeStyle = "blue";
 
-    let startButton = document.getElementById("start");
-    startButton.addEventListener("click", init)
+
+
+    startButton = document.getElementById("start");
+    startButton.addEventListener("click", init);
+    drawbutton = document.getElementById("draw");
+    drawbutton.addEventListener("click", update);
 
     
 }
 
+function update()
+{
+    if (document.getElementById("Theta1").value != "")
+        Theta1 = Number(document.getElementById("Theta1").value) * (Math.PI) / 180;
+    if (document.getElementById("Theta2").value != "")
+        Theta2 = Number(document.getElementById("Theta2").value) * (Math.PI) / 180;
+    if (document.getElementById("dTheta1").value != "")
+        dTheta1 = Number(document.getElementById("dTheta1").value) * (Math.PI) / 180;
+    if (document.getElementById("dTheta2").value != "")
+        dTheta2 = Number(document.getElementById("dTheta2").value) * (Math.PI) / 180;
 
+    if (document.getElementById("Theta1-2").value != "")
+        Theta1_2 = Number(document.getElementById("Theta1-2").value) * (Math.PI) / 180;
+    if (document.getElementById("Theta2-2").value != "")
+        Theta2_2 = Number(document.getElementById("Theta2-2").value) * (Math.PI) / 180;
+    if (document.getElementById("dTheta1-2").value != "")
+        dTheta1_2 = Number(document.getElementById("dTheta1-2").value) * (Math.PI) / 180;
+    if (document.getElementById("dTheta2-2").value != "")
+        dTheta2_2 = Number(document.getElementById("dTheta2-2").value) * (Math.PI) / 180;
+    
+    if (document.getElementById("Theta1-3").value != "")
+        Theta1_3 = Number(document.getElementById("Theta1-3").value) * (Math.PI) / 180;
+    if (document.getElementById("Theta2-3").value != "")
+        Theta2_3 = Number(document.getElementById("Theta2-3").value) * (Math.PI) / 180;
+    if (document.getElementById("dTheta1-3").value != "")
+        dTheta1_3 = Number(document.getElementById("dTheta1-3").value) * (Math.PI) / 180;
+    if (document.getElementById("dTheta2-3").value != "")
+        dTheta2_3 = Number(document.getElementById("dTheta2-3").value) * (Math.PI) / 180;
+
+    draw();
+}
 
 function init() 
 {
+    startButton.setAttribute("disabled", true);
+    drawbutton.setAttribute("disabled", true);
+    update();
     setInterval(function(){
         animate();
     }, 5);
@@ -153,6 +199,22 @@ function resize() {
 }
 
 function draw() {
+
+    ball1x = X0 + l1 * Math.sin(Theta1);
+    ball1y = Y0 + l1 * Math.cos(Theta1);
+    ball2x = X0 + l1 * Math.sin(Theta1) + l2 * Math.sin(Theta2);
+    ball2y = Y0 + l1 * Math.cos(Theta1) + l2 * Math.cos(Theta2);
+
+    ball1x_2 = X0 + l1_2 * Math.sin(Theta1_2);
+    ball1y_2 = Y0 + l1_2 * Math.cos(Theta1_2);
+    ball2x_2 = X0 + l1_2 * Math.sin(Theta1_2) + l2 * Math.sin(Theta2_2);
+    ball2y_2 = Y0 + l1_2 * Math.cos(Theta1_2) + l2 * Math.cos(Theta2_2);
+
+    ball1x_3 = X0 + l1_3 * Math.sin(Theta1_3);
+    ball1y_3 = Y0 + l1_3 * Math.cos(Theta1_3);
+    ball2x_3 = X0 + l1_3 * Math.sin(Theta1_3) + l2 * Math.sin(Theta2_3);
+    ball2y_3 = Y0 + l1_3 * Math.cos(Theta1_3) + l2 * Math.cos(Theta2_3);
+
     arm1.setAttribute("x2", ball1x);
     arm1.setAttribute("y2", ball1y);
     arm2.setAttribute("x1", ball1x);
@@ -199,10 +261,7 @@ function animate(myCircle1, myCircle2, myLine1, myLine2, canvas, context) {
     Theta1 += dTheta1 * time;
     Theta2 += dTheta2 * time;
 
-    ball1x = X0 + l1 * Math.sin(Theta1);
-    ball1y = Y0 + l1 * Math.cos(Theta1);
-    ball2x = X0 + l1 * Math.sin(Theta1) + l2 * Math.sin(Theta2);
-    ball2y = Y0 + l1 * Math.cos(Theta1) + l2 * Math.cos(Theta2);
+    
 
     mu_2 = 1 + m1_2 / m2_2;
     d2Theta1_2 = (g * (Math.sin(Theta2_2) * Math.cos(Theta1_2 - Theta2_2) - mu_2 * Math.sin(Theta1_2)) - (l2_2 * dTheta2_2 * dTheta2_2 + l1_2 * dTheta1_2 * dTheta1_2 * Math.cos(Theta1_2 - Theta2_2)) * Math.sin(Theta1_2 - Theta2_2)) / (l1_2 * (mu_2 - Math.cos(Theta1_2 - Theta2_2) * Math.cos(Theta1_2 - Theta2_2)));
@@ -212,10 +271,7 @@ function animate(myCircle1, myCircle2, myLine1, myLine2, canvas, context) {
     Theta1_2 += dTheta1_2 * time;
     Theta2_2 += dTheta2_2 * time;
 
-    ball1x_2 = X0 + l1_2 * Math.sin(Theta1_2);
-    ball1y_2 = Y0 + l1_2 * Math.cos(Theta1_2);
-    ball2x_2 = X0 + l1_2 * Math.sin(Theta1_2) + l2 * Math.sin(Theta2_2);
-    ball2y_2 = Y0 + l1_2 * Math.cos(Theta1_2) + l2 * Math.cos(Theta2_2);
+    
 
     mu_3 = 1 + m1_3 / m2_3;
     d2Theta1_3 = (g * (Math.sin(Theta2_3) * Math.cos(Theta1_3 - Theta2_3) - mu_3 * Math.sin(Theta1_3)) - (l2_3 * dTheta2_3 * dTheta2_3 + l1_3 * dTheta1_3 * dTheta1_3 * Math.cos(Theta1_3 - Theta2_3)) * Math.sin(Theta1_3 - Theta2_3)) / (l1_3 * (mu_3 - Math.cos(Theta1_3 - Theta2_3) * Math.cos(Theta1_3 - Theta2_3)));
@@ -225,10 +281,6 @@ function animate(myCircle1, myCircle2, myLine1, myLine2, canvas, context) {
     Theta1_3 += dTheta1_3 * time;
     Theta2_3 += dTheta2_3 * time;
 
-    ball1x_3 = X0 + l1_3 * Math.sin(Theta1_3);
-    ball1y_3 = Y0 + l1_3 * Math.cos(Theta1_3);
-    ball2x_3 = X0 + l1_3 * Math.sin(Theta1_3) + l2 * Math.sin(Theta2_3);
-    ball2y_3 = Y0 + l1_3 * Math.cos(Theta1_3) + l2 * Math.cos(Theta2_3);
 
     draw();
 }
